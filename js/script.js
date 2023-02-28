98ae9b64eec55c2c57943114e418cae648844f1f
Noiker-ux <71761947+Noiker-ux@users.noreply.github.com>
2023-02-28 11:38:06 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ function parallax(e) {
         parallax.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
 }
+
+$(document).ready(function(){
+    $.fn.animate_Text = function() {
+     var string = this.text();
+     return this.each(function(){
+      var $this = $(this);
+      $this.html(string.replace(/./g, '<span class="new">$&</span>'));
+      $this.find('span.new').each(function(i, el){
+       setTimeout(function(){ $(el).addClass('div_opacity'); }, 20 * i);
+      });
+     });
+    };
+    $('#example').show();
+    $('#example').animate_Text();
+   });
