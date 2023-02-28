bfeeb0070092398c0bcc04e7f113427458ba6eb1
Noiker-ux <71761947+Noiker-ux@users.noreply.github.com>
2023-02-28 15:35:01 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-document.addEventListener("mousemove", parallax);
+// document.addEventListener("mousemove", parallax);
 
-function parallax(e) {
-    this.querySelectorAll(".parallax").forEach(parallax => {
-        const speed = parallax.getAttribute("data-speed");
+// function parallax(e) {
+//     this.querySelectorAll(".parallax").forEach(parallax => {
+//         const speed = parallax.getAttribute("data-speed");
 
-        const x = (window.innerWidth - e.pageX * speed) / 100;
-        const y = (window.innerHeight - e.pageY * speed) / 100;
+//         const x = (window.innerWidth - e.pageX * speed) / 100;
+//         const y = (window.innerHeight - e.pageY * speed) / 100;
 
-        parallax.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    });
-}
+//         parallax.style.transform = `translateX(${x}px) translateY(${y}px)`;
+//     });
+// }
 
 $(document).ready(function(){
     $.fn.animate_Text = function() {
@@ -25,3 +25,11 @@ $(document).ready(function(){
     $('#example').show();
     $('#example').animate_Text();
    });
+
+window.onload = function() {
+    setTimeout(function() {
+        // document.getElementById("preloader_malc").style.display = "none";
+        $("#preloader_malc").addClass('hidden_prel');
+    }, 2200);
+
+};
